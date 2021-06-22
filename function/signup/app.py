7d9bfb1c3f0b57246758c89c55d424af8cd24106
sonyj <sonik0627@naver.com>
2021-06-22 09:30:58 +0900

    push

--- function/signup/app.py
+++ function/signup/app.py
@@ -11,7 +11,7 @@ def index():
 
 
 
-@app.route("/signup", methods=["post"])
+@app.route("/signup", methods=["POST"])
 def insertuser():
     dao = USERDAO()
     dto = USERDTO(request.form.get("u_id"), request.form.get("u_pw"), request.form.get("nick"))
@@ -19,6 +19,22 @@ def insertuser():
 
     return jsonify(result=200)
 
+@app.route('/checkid', methods = ["POST"])
+def selectid():
+    dao = USERDAO()
+    dto = USERDTO(request.form.get("u_id"))
+    dao.userinsert(dto)
+
+    return jsonify(result=200)
+
+@app.route('/checknick', methods = ["POST"])
+def selectnick():
+    dao = USERDAO()
+    dto = USERDTO(request.form.get("nick"))
+    dao.userinsert(dto)
+
+    return jsonify(result=200)
+    
 
 if __name__=='__main__':
     app.run(debug=True, host="127.0.0.1", port="5000")
