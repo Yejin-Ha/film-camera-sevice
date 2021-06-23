5e7e283274740c1d9e2e0e890c1467178706871c
Hyunsoo-Ryan-Lee <83285291+Hyunsoo-Ryan-Lee@users.noreply.github.com>
2021-06-23 11:28:16 +0900

    주석 삭제

--- function/camera_search/app.py
+++ function/camera_search/app.py
@@ -13,26 +13,6 @@ def getBrand():
     dao = CameraDAO()
     return dao.allCams(request.form.get('brand'),request.form.get('category'),request.form.get('test_level'))
 
-# @app.route('/addCams',methods=['POST'])
-# def addCams():
-#     dao = CameraDAO()
-#     dto = CameraDTO(request.form.get('brand'),request.form.get('model'),request.form.get('price'),request.form.get('format'))
-#     dao.addCams(dto)
-#     return dao.allCams(request.form.get('brand'))
-
-# @app.route('/delCams',methods=['POST'])
-# def delCams():
-#     dao = CameraDAO()
-#     dto = CameraDTO(request.form.get('brand'),request.form.get('model'),0,0)
-#     dao.delCams(dto)
-#     return dao.allCams(request.form.get('brand'))
-
-@app.route('/showall',methods=['GET'])
-def showall():
-    dao = CameraDAO()
-    return dao.showall()
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port="5000")
