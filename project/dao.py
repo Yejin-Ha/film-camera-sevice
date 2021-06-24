5edbb16afbf483e37fda41ab16b13b93ea9c755d
hyj <hyj1004mu@naver.com>
2021-06-24 10:49:40 +0900

    pop up 기능 병합

--- project/dao.py
+++ project/dao.py
@@ -5,6 +5,38 @@ import json
 import collections
 
 class Camera:
+    def allFilms(self):
+        film_data = []
+        try:
+            conn = cx_Oracle.connect(user="SCOTT", password="TIGER", dsn="xe")
+            cur = conn.cursor()
+
+            try:
+                cur.execute("select * from films")
+                rows = cur.fetchall()
+                v = []
+                for row in rows:
+                    d = collections.OrderedDict()
+                    d['film_brand'] = row[0]
+                    d['film_name'] = row[1]
+                    d['film_type'] = row[2]
+                    d['iso'] = row[3]
+                    v.append(d)
+                # print(v)
+                film_data = json.dumps(v,ensure_ascii=False)
+                       
+            except Exception as e:
+                print(e)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            cur.close()
+            conn.close()
+
+        return film_data 
+
     def allCams(self,brand,category,test_level):
         cam_data = []
         try:
